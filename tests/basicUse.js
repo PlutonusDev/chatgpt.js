602ec95c2f1e62f46a0c8203423e53fdd8851553
Joshua Hughes <joshua@plutonus.dev>
2022-12-11 20:36:18 +1100

    Fix formatting

--- tests/basicUse.js
+++ tests/basicUse.js
@@ -6,13 +6,13 @@ const chatgpt = new GPTChat.Client({ authToken });
 
 chatgpt.once("ready", async () => {
     let resp = await chatgpt.call("what is the scientific name for the common house finch?").catch(e => console.log(e));
-    if(!resp) return console.error("looks like something went wrong :(");
+    if (!resp) return console.error("looks like something went wrong :(");
     console.log(resp);
 
     await sleep(10000);
-    
+
     resp = await chatgpt.call("are there any in Australia?", resp.conversation).catch(e => console.log(e));
-    if(!resp) return console.error("looks like something went wrong :(");
+    if (!resp) return console.error("looks like something went wrong :(");
     console.log(resp);
 });
 
